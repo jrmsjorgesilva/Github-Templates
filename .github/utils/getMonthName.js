20f53e757b0888cf7d560dbc802acab51287070d
jorge machado <jrms_jorgesilva@hotmail.com>
2022-10-28 15:06:30 -0300

    feat: getMOnthName utility

---
+++ .github/utils/getMonthName.js
@@ -0,0 +1,45 @@
+const months = [
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro",
+];
+
+const monthsAbbreviated = [
+  "Jan",
+  "Fev",
+  "Mar",
+  "Abr",
+  "Mai",
+  "Jun",
+  "Jul",
+  "Ago",
+  "Set",
+  "Out",
+  "Nov",
+  "Dez",
+];
+
+const weekday = [
+  "Domingo",
+  "Segunda-Feira",
+  "Terça-Feira",
+  "Quarta-Feira",
+  "Quinta-Feira",
+  "Sexta-Feira",
+  "Sábado",
+];
+
+const getMonthName = () => {
+  return;
+};
+
+export default getMonthName;
